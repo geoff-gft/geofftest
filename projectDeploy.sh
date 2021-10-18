01b2637bd6ef5f80703760313f73103830ed7ce7
Geoff <73950019+geoff-gft@users.noreply.github.com>
2021-10-18 14:06:10 +0100

    Update projectDeploy.sh

--- projectDeploy.sh
+++ projectDeploy.sh
@@ -3,18 +3,17 @@ set -e
 #RANDOM="$(dd if=/dev/urandom count=5 bs=1 | od -t d)"
 #create a project and save it to a variable
 RANDOM_NUM=${RANDOM}
-NEW_PROJECT="faisal-cicd-21607"
+NEW_PROJECT="faisal-cicd-${RANDOM_NUM}"
 
 echo "1"
-# gcloud projects create ${NEW_PROJECT} --folder=398506523923
+gcloud projects create ${NEW_PROJECT} --folder=398506523923
 echo "2"
 gcloud config set project ${NEW_PROJECT}
 echo "3"
-# gcloud config set compute/zone europe-west1-b
+gcloud config set compute/zone europe-west1-b
 echo "4"
 gcloud services enable cloudbilling.googleapis.com
 echo "5"
-sleep 20
 gcloud alpha billing projects link "${NEW_PROJECT}" --billing-account "01A2F5-73127B-50AE5B" --format=none
 echo "6"
 gcloud services enable compute.googleapis.com cloudresourcemanager.googleapis.com
