35d9aa71ea16fb76aa272f8dc815512b1d42bc97
Geoff <73950019+geoff-gft@users.noreply.github.com>
2021-10-21 10:13:34 +0100

    Update projectDeploy.sh

--- projectDeploy.sh
+++ projectDeploy.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 set -e
-uname -srm
-uname --kernel-name --kernel-release --machine
 
 apt-get update -y
-echo "upgrade"
+echo "-upgrade-"
 apt-get upgrade -y
-echo "install unzip"
+echo "-install unzip-"
 apt-get install unzip -y
-echo "Install wget"
-install wget
+echo "-Install wget-"
+apt-get install wget -y
 
-echo "dl"
+echo "-dl-"
 wget https://releases.hashicorp.com/terraform/0.13.1/terraform_0.13.1_linux_amd64.zip
 
-echo "unzip"
+echo "-unzip-"
 unzip terraform_0.13.1_linux_amd64.zip
 
-echo "mv"
+echo "-mv-"
 mv terraform /usr/local/bin/
 terraform --version
 
-echo "Remove zip file---------------"
-rm -f terraform_1.0.9_linux_amd64.zip
-
-
-
-VERSION_INSTALLED="$(terraform -version)"
-echo "Version installed: ${VERSION_INSTALLED}"
